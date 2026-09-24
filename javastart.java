8683b016c1cbb1de931156e1a154946f6a61270c
My name <me@example.com>
2026-09-24 23:46:31 +0500

    another print line added

--- javastart.java
+++ javastart.java
@@ -1,5 +1,6 @@
 public class javastart{
 	public static void main(String[] args){
 		System.out.print("System starting .... user is:" + args[0]);
+		System.out.print("Git repo added to github");
 	}
 }
